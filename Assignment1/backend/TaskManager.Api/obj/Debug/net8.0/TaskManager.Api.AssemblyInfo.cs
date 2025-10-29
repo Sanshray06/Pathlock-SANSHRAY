e847a8837b1761c0bfe1dfe6a52de5291742e19b
Sanshray06 <sanshraymittu6@gmail.com>
2025-10-29 19:46:15 +0530

    Initial commit: Task Manager app with modern UI

--- Assignment1/backend/TaskManager.Api/obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ Assignment1/backend/TaskManager.Api/obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd378c6703c9a57205680212d83d88384f72992")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
